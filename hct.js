e5f36b74c1c3f442b7ee8b288d847bf4429b280f
hct-14 <99867342+hct-14@users.noreply.github.com>
2023-03-01 22:31:54 +0700

    Add files via upload

---
+++ hct.js
@@ -0,0 +1,9 @@
+function run(sum, different) {
+    var a = 6;
+    var b = 4;
+    sum = a + b;
+    different = a - b;
+    console.log('a is ' + a + " and b is " + b);
+    console.log('sum is ' + sum + " and different is " + different);
+  }
+  
